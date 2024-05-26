a63aab1088885d11f8d3438eff993ced53761b62
murilofray <murilofray01@gmail.com>
2024-05-25 22:00:18 -0300

    Refatorando Funcoes.py

--- backend/funcoes.py
+++ backend/funcoes.py
@@ -1,30 +1,28 @@
 from backend.db import mysql
-from flask import Flask
 
-# Crie uma instância do aplicativo Flask
-app = Flask(__name__)
+# Extrai o código SQL da mensagem
+def extrair_sql(mensagem):
+    partes_sql = mensagem.split("CODIGO SQL121:")
+    codigo_sql = " ".join(partes_sql)
+    codigo_sql = codigo_sql.replace("```", "").strip()
+    codigo_sql = codigo_sql.replace("CODIGO SQL121:", "").strip()
+    return mensagem
 
-# Função para extrair parâmetros de uma mensagem
-def extrair_parametros(partes):
-    parametros = []
-    for parte in partes:
-        if "(" in parte and ")" in parte:
-            parametros_str = parte[parte.find("(") + 1: parte.find(")")]
-            parametros.extend(parametros_str.split(","))
-    return parametros
-
+# Função para processar e executar um comando SQL a partir de uma mensagem
 def processar_sql(mensagem):
     try:
-        codigo_sql = mensagem.split("CODIGO SQL121:")
-        codigo_sql = " ".join(codigo_sql)
-        codigo_sql = codigo_sql.replace("```", "").strip()
-        codigo_sql = codigo_sql.replace("CODIGO SQL121:", "").strip()
-        codigo_sql = codigo_sql[codigo_sql.find("SELECT"):]
-        cursor = mysql.connection.cursor()
-        cursor.execute(codigo_sql)
-        resultado = cursor.fetchall()
-        cursor.close()
-        resultado_final = '\n'.join([str(a) for a in resultado])
-        return resultado_final
+        codigo_sql = extrair_sql(mensagem)
+        # Encontra a posição da palavra "SELECT" para garantir que o comando SQL seja válido e que não seja um delete ou update
+        if "SELECT" in codigo_sql:
+            codigo_sql = codigo_sql[codigo_sql.find("SELECT"):]
+            # Executa o comando SQL
+            cursor = mysql.connection.cursor()
+            cursor.execute(codigo_sql)
+            resultado = cursor.fetchall()
+            cursor.close()
+            # Formata o resultado para uma string legível
+            resultado_final = '\n'.join([str(linha) for linha in resultado])
+            return resultado_final
+        return "Comando SQL inválido."
     except Exception as e:
         return "Erro ao executar o código SQL: " + str(e)
